8cf838d734adf6e7217a1f859664a036d4e75630
Luigi Fraccalvieri <l.fraccalvieri4@studenti.uniba.it>
2023-06-27 10:38:35 +0200

    adsasdaee

--- app/src/main/java/com/example/appvillaggioturistico/GuardianFormActivity.kt
+++ app/src/main/java/com/example/appvillaggioturistico/GuardianFormActivity.kt
@@ -2,6 +2,7 @@ package com.example.appvillaggioturistico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -9,10 +10,15 @@ import android.widget.Toast
 import androidx.core.view.WindowCompat
 import com.example.appvillaggioturistico.databinding.ActivityGuardianFormBinding
 import com.example.appvillaggioturistico.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class GuardianFormActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGuardianFormBinding
+
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -20,6 +26,14 @@ class GuardianFormActivity : AppCompatActivity() {
         binding = ActivityGuardianFormBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        database = Firebase.database.reference
+
+        database.child("guardiano").get().addOnSuccessListener {
+            Log.i("firebase", "Got value ${it.value}")
+        }.addOnFailureListener{
+            Log.e("firebase", "Error getting data", it)
+        }
 
         val guardiani = arrayOf("", "Alice", "Bob", "Charlie", "Dave")
 
